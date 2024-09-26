aefa14093975d996f3d0993b2af75877edfe00d0
Norah240 <nalshehri24@twq.idserve.net>
2024-09-26 17:06:34 +0300

    Initial Commit

---
+++ challenge1/challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge1App.swift
+//  challenge1
+//
+//  Created by Norah mo on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
